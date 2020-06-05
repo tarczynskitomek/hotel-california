0528e9d1d63b20732e80fae316c0fbcf67b6f2d8
Tomasz Tarczyński <tomasz.tarczynski@dbschenker.com>
2020-06-05 08:39:34 +0200

    Initial commit

---
+++ src/main/kotlin/it/tarczynski/hotelcalifornia/HotelcaliforniaApplication.kt
@@ -0,0 +1,11 @@
+package it.tarczynski.hotelcalifornia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HotelcaliforniaApplication
+
+fun main(args: Array<String>) {
+	runApplication<HotelcaliforniaApplication>(*args)
+}
---
+++ src/test/kotlin/it/tarczynski/hotelcalifornia/HotelcaliforniaApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.tarczynski.hotelcalifornia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelcaliforniaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
